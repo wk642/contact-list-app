671d2a1770edfb7c75a30e18e8c365808ad01ec9
Winnie Kelley <w642kelley@gmail.com>
2025-03-25 15:41:10 -0500

    createed database

---
+++ server/database/db.sql
@@ -0,0 +1,9 @@
+-- create database
+
+-- DROP the database first in case there's an existing one
+DROP DATABASE 'wkcontactlistapp';
+-- CREATE the database now
+CREATE DATABASE "wkcontactlistapp";
+
+-- connect to the database
+\connect wkcontactlistapp;
