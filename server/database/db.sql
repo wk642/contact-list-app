849a3e4e2da6b14f6f4ef3769b57560a9c1cb118
Winnie Kelley <w642kelley@gmail.com>
2025-03-25 17:06:57 -0500

    Successfully created db, tables and added mock data.

--- server/database/db.sql
+++ server/database/db.sql
@@ -1,14 +1,22 @@
 -- create database
 
 -- DROP the database first in case there's an existing one
-DROP DATABASE 'wkcontactlistapp';
+DROP DATABASE wkcontactlistapp;
 -- CREATE the database now
-CREATE DATABASE "wkcontactlistapp";
+CREATE DATABASE wkcontactlistapp;
 
 -- connect to the database
 \connect wkcontactlistapp;
 
--- CREATE tables
+-- CREATE tables 
+-- GROUPS -- have to create this first, or else the foreign key linking is not going to work.
+CREATE TABLE IF NOT EXISTS groups (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  group_name VARCHAR,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 -- CONTACTS
 CREATE TABLE IF NOT EXISTS contacts (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -18,28 +26,20 @@ CREATE TABLE IF NOT EXISTS contacts (
   phone_number VARCHAR(15),
   notes text,
   group_id UUID REFERENCES groups(id),
-  profile_image VARBINARY(MAX), 
-  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-};
-
--- GROUPS
-CREATE TABLE IF NOT EXISTS groups (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  group_name VARCHAR,
+  profile_image BYTEA, --https://risingwave.com/blog/introduction-to-postgresql-bytea-data-type/
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Inserting minimum of 3 mock data
--- CONTACTS
-INSERT INTO contacts (first_name, last_name, email, phone_number, notes, profile_image, group_id) VALUES
- ('Winnie', 'Kelley', "wk01@gmail.com", '555-111-1111', 'This is just a mock contact', NULL, (SELECT id FROM groups WHERE group_name = 'Family'), NULL),
- ('John', 'Dough', "wk02@gmail.com", '555-111-1112', NULL, NULL, (SELECT id FROM groups WHERE group_name = 'Work'), NULL),
- ('Jane', 'Dough', "wk03@gmail.com", '555-111-1113', 'Besties!!!', NULL, (SELECT id FROM groups WHERE group_name = 'Friends'), NULL);
-
  -- GROUPS
  INSERT INTO groups (group_name) VALUES
  ('Family'),
  ('Work'),
  ('Friends');
+
+-- CONTACTS
+INSERT INTO contacts (first_name, last_name, email, phone_number, notes, profile_image, group_id) VALUES
+ ('Winnie', 'Kelley', 'wk01@gmail.com', '555-111-1111', 'This is just a mock contact', NULL, (SELECT id FROM groups WHERE group_name = 'Family')),
+ ('John', 'Dough', 'wk02@gmail.com', '555-111-1112', NULL, NULL, (SELECT id FROM groups WHERE group_name = 'Work')),
+ ('Jane', 'Dough', 'wk03@gmail.com', '555-111-1113', 'Besties!!!', NULL, (SELECT id FROM groups WHERE group_name = 'Friends'));
