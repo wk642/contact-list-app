e36b53aa1c5d2b495e81300780c1e0e34b161379
Winnie Kelley <w642kelley@gmail.com>
2025-03-27 12:53:18 -0500

    Added magnifying lass icon

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,17 +1,21 @@
-import { MagicWandIcon } from '@radix-ui/react-icons';
+import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
 import React from 'react';
 
 // exporting up here due to suggestion, will go in and change the rest up as well later
 function SearchBar({ searchContact, onChangeSearch }) {
   return (
-    <div className="p-4 fixed top-0 left-0 w-full bg-black text-slate-500 z-10 shadow-md">
-      <input
-        type="text"
-        placeholder="Search ... "
-        value={searchContact}
-        onChange={onChangeSearch}
-        className="border-b-4 p-5 w-full text-white"
-      />
+    <div className="p-4 fixed top-0 left-0 w-full bg-black text-slate-500 shadow-md">
+      <div className="relative flex items-center">
+        {/* Want to make sure that the magnifying glass is on the same line as the search  */}
+        <MagnifyingGlassIcon className="h-5 w-5 absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-500" />
+        <input
+          type="text"
+          placeholder="Search ... "
+          value={searchContact}
+          onChange={onChangeSearch}
+          className="border-b-4 p-5 pl-8 w-full text-white bg-black"
+        />
+      </div>
     </div>
   );
 }
